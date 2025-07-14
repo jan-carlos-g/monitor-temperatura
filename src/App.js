e6d5bec693cd3c220cf425bddb06e79d5edb42c5
Jan Carlos <jan13spfc@hotmail.com>
2025-07-13 21:46:57 -0300

    New link ngrok

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ ChartJS.register(
   Legend
 );
 
-const API_URL = "https://d8f9bf316cfd.ngrok-free.app/api/dados";
+const API_URL = "https://2cd1ee0b175c.ngrok-free.app/api/dados";
 
 export default function App() {
   const [dados, setDados] = useState([]);
